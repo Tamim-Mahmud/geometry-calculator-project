7c9b4d9bca4d4417134ab56a021275d5297fc385
TamimMahmud <tmahmud.jnubd@gmail.com>
2023-06-17 00:12:28 +0600

    error solved

--- js/calculate.js
+++ js/calculate.js
@@ -19,14 +19,34 @@ for (const calculateBtn of allCalculateBtn) {
         <div class="flex justify-between mt-5 items-center">
                         <p>${count}.</p>
                         <p>${shapeType}</p>
-                        <p>
+                        <p id="result-for-m">
                             <span>${result}</span>
                             <span>cm<sup>2</sup></span>
                         </p>
-                        <button class="bg-blue-600 px-3 py-2 text-white rounded-md">Convert to m <sup>2</sup></button>
-                    </div>
+                        <button class="convert-btn bg-blue-600 px-3 py-2 text-white rounded-md">Convert to m <sup>2</sup></button>
+        </div>
         `
             document.getElementById('result-area').appendChild(newElement);
+        }
+
+        //convet-btn
+        const allConvertBtn = document.getElementsByClassName('convert-btn');
+        let convertCount = 1;
+        for (const convertBtn of allConvertBtn) {
+            convertBtn.addEventListener('click', function () {
+                if (convertCount === 1) {
+                    const valueInM = (parseFloat(convertBtn.parentNode.childNodes[5].childNodes[1].innerText) * 0.0001).toFixed(4);
+                    convertCount++;
+                    convertBtn.parentNode.childNodes[5].innerHTML = ``;
+                    convertBtn.parentNode.childNodes[5].innerHTML = `
+                <span>${valueInM}</span>
+                <span>m<sup>2</sup></span>
+                `
+                }
+                else{
+                    alert('Already Converted to meter Square');
+                }
+            });
         }
 
 
